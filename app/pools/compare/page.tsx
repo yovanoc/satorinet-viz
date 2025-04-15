0859fe6ec47194561733da9f98bf3a0f965880ad
Christopher Yovanovitch <yovano_c@outlook.com>
2025-04-15 17:37:44 +0200

    fix: revalidate compare page

--- app/pools/compare/page.tsx
+++ app/pools/compare/page.tsx
@@ -1,5 +1,7 @@
 import MultiPoolWorkerComparison from "@/components/old/multi-pool-worker-comparison";
 
+export const revalidate = 3_600; // 1 hour
+
 export default function PoolsCompare() {
   return (
     <div className="flex flex-col py-4 gap-4 px-4 lg:px-6 h-full">
