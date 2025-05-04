b41cce4b196c18fbe82549cf1632b2fc23666910
Christopher Yovanovitch <yovano_c@outlook.com>
2025-05-04 12:41:33 +0200

    feat: display base address

--- app/address/[address]/components/base.tsx
+++ app/address/[address]/components/base.tsx
@@ -13,6 +13,11 @@ export async function BaseAddress({ address }: { address: string }) {
 
   return (
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+      <div className="flex h-full items-center gap-4">
+        <div className="text-muted-foreground text-sm break-all">
+          Address: {baseAddress}
+        </div>
+      </div>
       <div className="flex h-full">
         <div className="text-primary text-2xl">
           <a
