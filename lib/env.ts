7d04eb08c38444356757bb31d526d028eac6e277
Christopher Yovanovitch <yovano_c@outlook.com>
2025-05-31 22:55:24 +0200

    fix: build

--- lib/env.ts
+++ lib/env.ts
@@ -6,18 +6,12 @@ export const env = createEnv({
     DATABASE_URL: z.string().url(),
     LIVECOINWATCH_API_KEY: z.string(),
     REDIS_URL: z.string().url(),
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
   },
   client: {
     //
   },
-  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
-  runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    LIVECOINWATCH_API_KEY: process.env.LIVECOINWATCH_API_KEY,
-    REDIS_URL: process.env.REDIS_URL,
-  },
-  // For Next.js >= 13.4.4, you only need to destructure client variables:
-  // experimental__runtimeEnv: {
-  //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
-  // }
+  experimental__runtimeEnv: {
+    //
+  }
 });
